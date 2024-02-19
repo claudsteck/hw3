cb8e8937403be16a4b0a9b63d91eefbfce1cad35
Claudia Steck <claudia.steck@kellogg.northwestern.edu>
2024-02-19 01:18:43 +0000

    commit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,6 +7,22 @@ class PlacesController < ApplicationController
   render :template => 'places/index'
   end
 
-  
+  def new
+    # render view with new places form
+  end
 
+  def create
+    # start with a new place
+    @place = Place.new
+
+    # assign user-entered form data to columns
+    @place["name"] = params["name"]
+    
+    # save  row
+    @place.save
+
+    # redirect user
+    redirect_to "/places"
+  end
 end
+  
