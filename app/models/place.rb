65190745359de4a3061c1fe4fca0fca4e9ef0733
Claudia Steck <claudia.steck@kellogg.northwestern.edu>
2024-02-19 00:57:24 +0000

    commit

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,18 +1,3 @@
 class Place < ApplicationRecord
   # talks to the "places" table
-  
-  Place.destroy_all
-
-evanston = Place.new
-evanston.name = "Evanston, IL"
-evanston.save
-
-new_york_city = Place.new
-new_york_city.name = "New York, NY"
-new_york_city.save
-
-mexico_city = Place.new
-mexico_city.name = "Mexico City, MX"
-mexico_city.save
-
 end
